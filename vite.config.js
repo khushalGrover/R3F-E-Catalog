9099333e6f483e98da62497e92d61bb4f109ac6e
khushalGrover <khushal15grover@gmail.com>
2025-11-11 18:55:40 +0530

    base url change

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'r3f-e-catalog',
+  base: '/',
   plugins: [react()],
   server: {
     host: true
